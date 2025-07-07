1d895314ccbad9d5bccff557cd91df1cfd15e06c
Stevetatu <stevetatu3@gmail.com>
2025-07-07 23:15:42 +0300

    completing minigrep cli components

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,51 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process::exit;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // Query file name from command line arguments
-    let query = &args[1];
-    let filename = &args[2];
+    let config= Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        exit(1);
+    });
 
     // Print the query and filename
-    println!("searching for: {}", query);
-    println!("In file {}", filename);
+    println!("searching for: {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+   if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        exit(1);
+    
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("File contents:\n{}", contents);
+
+    Ok(())
 }
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments provided. Usage: <query> <filename>");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename})
+
+    }
+    
+}
+
