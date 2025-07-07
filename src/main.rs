d1461d351fcfa9e321bbb6c425e177b058b8002d
Stevetatu <stevetatu3@gmail.com>
2025-07-07 20:37:41 +0300

    A Poem to the cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    // Query file name from command line arguments
+    let query = &args[1];
+    let filename = &args[2];
+
+    // Print the query and filename
+    println!("searching for: {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("File contents:\n{}", contents);
 }
